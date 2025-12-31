5e792bb561f2b664984e83e4afbc43178086fa55
bruhmmnt <162716920+bruh-moment-0@users.noreply.github.com>
2025-12-31 12:07:38 +0300

    client V2.2.1 manual upload

--- aes256.py
+++ aes256.py
@@ -1,3 +1,21 @@
+# The AES-256-GCM and Argon2id code of Private Safe Messaging Client
+# Copyright (C) 2025  bruh-moment-0
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
 from argon2.low_level import hash_secret_raw, Type # argon2id for AES256GCM
 from Crypto.Cipher import AES # AES256GCM
 from Crypto.Random import get_random_bytes # salt/nonce for AES256GCM
