43314e340d23ceb40eea1a2c8d7b4327511b1e78
bruhmmnt <162716920+bruh-moment-0@users.noreply.github.com>
2025-12-31 12:11:03 +0300

    client V2.2.1 manual upload

--- data.py
+++ data.py
@@ -1,3 +1,21 @@
+# The file management system code of Private Safe Messaging Client
+# Copyright (C) 2025  bruh-moment-0
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
 # data.py but for clients!
 from const import * # constants
 from typing import Any
@@ -6,6 +24,7 @@ import json # you guessed it, the best db ever
 import zlib
 import os
 
+VERSION = "CLIENT V2.2.1 (built 9:00 GMT+0 31/12/2025)"
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 STORAGEDIR = os.path.join(BASEDIR, "storage")
 FILESDIR = os.path.join(STORAGEDIR, "files")
