2b4ca577bb12a99e356127cd895e23912100ae61
bruhmmnt <162716920+bruh-moment-0@users.noreply.github.com>
2025-12-31 12:14:44 +0300

    client V2.2.1 manual upload

--- unsafeadapter.py
+++ unsafeadapter.py
@@ -1,5 +1,27 @@
+# The unsafe internet adapter code of Private Safe Messaging Client
+# Copyright (C) 2025  bruh-moment-0
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
 # WARNING, THIS ADAPTER IS NOT SAFE AND IS VULNERABLE TO MITM ATTACKS.
 # DO NOT USE THIS IF NOT ABSOLUTELY NECESSARY!!!
+
+# NOTICE TO READER: this is only activated if your internet connection is being controlled (school, library networks etc.) and disable all cert failsafes
+# which allows the IT managers to be able to read the requests. however, the critical information is still being encrypted and is safe. without this,
+# requests in those limited networks dont sadly work.
 
 import requests
 from requests.adapters import HTTPAdapter
