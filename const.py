81bb4eedfd4f618a86b398ce773ff4ba1246b243
bruhmmnt <162716920+bruh-moment-0@users.noreply.github.com>
2025-12-31 12:09:11 +0300

    client V2.2.1 manual upload

--- const.py
+++ const.py
@@ -1,11 +1,25 @@
+# constants of Private Safe Messaging Client
+# Copyright (C) 2025  bruh-moment-0
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
 # CONSTANTS FOR PRIVATE SAFE MESSAGING
 # DO NOT MODIFY IF YOU DONT KNOW WHAT YOU ARE DOING
 
-# for almost all python files
-# version
-VERSION = "CLIENT V2.2.0 (built 11:10 GMT+0 28/11/2025)"
-
-# for link.py, hashchecker.py
+# for link.py
 # default:
 # DEMOMODE = False
 # used only when the api is tested locally.
@@ -23,7 +37,7 @@ MULTIPLE_PORTS = True
 # the default port
 PORT_BASE = 8080
 
-# for link.py and hashchecker.py
+# for link.py
 # for getting the api url and hashes
 URLBASE = "https://raw.githubusercontent.com/bruh-moment-0/psm-util/refs/heads/main/"
 
