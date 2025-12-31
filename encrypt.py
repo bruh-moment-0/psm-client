5ef58f2fec389add7e4d75d174251bf7ac1e5635
bruhmmnt <162716920+bruh-moment-0@users.noreply.github.com>
2025-12-31 12:10:13 +0300

    client V2.2.1 manual upload

--- encrypt.py
+++ encrypt.py
@@ -1,3 +1,21 @@
+# The encap and decap code of Private Safe Messaging Client
+# Copyright (C) 2025  bruh-moment-0
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives import hashes
 from quantum import oqs, encap, decap # custom
